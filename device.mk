83533afe2b8b335f288baeb52cc63bc3a081c112
ThEMarD  <>
2024-04-14 00:05:17 -0300

    spes: Switch to AOSP WFD
     * Remove QCOM WFD implementation as it doesn't work as good as AOSP WFD.
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -554,18 +554,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor \
-    libjsoncpp.vendor \
-    libpng.vendor
-
-ifdef CR_VERSION
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-endif
-
 # XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts
