91d8159b5aa9eb2cd6ee3eb1a41884252f892a90
TheMatheusDev <apenasmatheus1@gmail.com>
2024-08-06 22:23:23 -0300

    spes: overlay: Refactor all overlays to RRO
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -377,6 +377,17 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    SpesApertureOverlay \
+    SpesCarrierConfigOverlay \
+    SpesDialerOverlay \
+    SpesFrameworksOverlay \
+    SpesLineageSDKOverlay \
+    SpesSettingsOverlay \
+    SpesSettingsProviderOverlay \
+    SpesSystemUIOverlay \
+    SpesTelephonyOverlay \
+    SpesWifiOverlay
 
 # Power
 PRODUCT_PACKAGES += \
@@ -465,7 +476,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlay \
     extphonelib \
     extphonelib-product \
     extphonelib.xml \
@@ -516,7 +526,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
