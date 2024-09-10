a075db3a9c4b7c19fa2990abbbf1a61906373dac
lahaina <nattolecats@gmail.com>
2024-09-10 03:21:07 +0000

    spes: Link libwvhidl against libcrypto from vndk 33
    * It depends on symbol CBS_init which is removed from the latest libcrypto.
    
    Change-Id: I4fb1bf4502590ea0dab13d5b930acc178f831f64
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -60,6 +60,9 @@ function blob_fixup() {
         vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
             llvm-strip --strip-debug "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+            ;;
     esac
 }
 
